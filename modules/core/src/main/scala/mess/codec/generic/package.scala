d4fd334aa9135881a41369d1f1e957571e4f11b4
Takeru Sato <type.in.type@gmail.com>
2019-08-13 00:12:57 +0900

    Drop export-hook

--- modules/core/src/main/scala/mess/codec/generic/package.scala
+++
@@ -1,9 +0,0 @@
-package mess.codec
-
-import shapeless.Lazy
-
-package object generic {
-
-  def derivedEncoder[A](implicit A: Lazy[DerivedEncoder[A]]): DerivedEncoder[A] = A.value
-  def derivedDecoder[A](implicit A: Lazy[DerivedDecoder[A]]): DerivedDecoder[A] = A.value
-}
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,24 +5,19 @@ object Dependencies {
     val `scala2.13`   = "2.13.0-RC1"
     val `scala2.12`   = "2.12.8"
     val shapeless     = "2.3.3"
-    val exportHook    = "1.2.0"
     val scalacheck    = "1.14.0"
     val scalatest     = "3.0.7"
     val scalatestSnap = "3.0.8-RC2"
     val msgpackJava   = "0.8.16"
-    val macroParadise = "2.1.1"
   }
 
   val Pkg = new {
-    lazy val shapeless     = "com.chuusai" %% "shapeless" % Ver.shapeless
-    lazy val exportHook    = "org.typelevel" %% "export-hook" % Ver.exportHook
-    lazy val scalacheck    = "org.scalacheck" %% "scalacheck" % Ver.scalacheck
-    lazy val msgpackJava   = "org.msgpack" % "msgpack-core" % Ver.msgpackJava
-    lazy val macroParadise = ("org.scalamacros" % "paradise" % Ver.macroParadise).cross(CrossVersion.patch)
+    lazy val shapeless   = "com.chuusai"    %% "shapeless"   % Ver.shapeless
+    lazy val scalacheck  = "org.scalacheck" %% "scalacheck"  % Ver.scalacheck
+    lazy val msgpackJava = "org.msgpack"    % "msgpack-core" % Ver.msgpackJava
 
     def scalatest(v: String) =
       "org.scalatest" %% "scalatest" % (if (v == Ver.`scala2.13`) Ver.scalatestSnap else Ver.scalatest)
-    def scalaReflect(v: String) = "org.scala-lang" % "scala-reflect" % v % Provided
-    def forTest(v: String)      = Seq(scalatest(v), scalacheck).map(_ % Test)
+    def forTest(v: String) = Seq(scalatest(v), scalacheck).map(_ % Test)
   }
 }
