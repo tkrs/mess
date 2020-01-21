0c2d1550635938d8dfc954436797f7d1b8af647a
Takeru Sato <type.in.type@gmail.com>
2020-01-21 21:25:58 +0900

    chore(deps): switch to scalacheck-1-14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,18 +2,19 @@ import sbt._
 
 object Dependencies {
   val Ver = new {
-    val `scala2.13`   = "2.13.1"
-    val `scala2.12`   = "2.12.10"
+    val `scala2.13` = "2.13.1"
+    val `scala2.12` = "2.12.10"
+
     val shapeless     = "2.3.3"
-    val scalatest     = "3.1.0"
-    val scalatestplus = "3.1.0.0-RC2"
     val msgpackJava   = "0.8.20"
+    val scalatest     = "3.1.0"
+    val scalatestplus = "3.1.0.1"
   }
 
   val Pkg = new {
-    lazy val shapeless   = "com.chuusai"       %% "shapeless"                % Ver.shapeless
-    lazy val scalatest   = "org.scalatest"     %% "scalatest"                % Ver.scalatest
-    lazy val scalacheck  = "org.scalatestplus" %% "scalatestplus-scalacheck" % Ver.scalatestplus
-    lazy val msgpackJava = "org.msgpack"       % "msgpack-core"              % Ver.msgpackJava
+    lazy val shapeless   = "com.chuusai"       %% "shapeless"       % Ver.shapeless
+    lazy val msgpackJava = "org.msgpack"       % "msgpack-core"     % Ver.msgpackJava
+    lazy val scalatest   = "org.scalatest"     %% "scalatest"       % Ver.scalatest
+    lazy val scalacheck  = "org.scalatestplus" %% "scalacheck-1-14" % Ver.scalatestplus
   }
 }
