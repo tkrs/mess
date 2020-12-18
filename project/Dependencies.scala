0f8132ca06dfb63a1bc1a202cce959b5bd5a0ef5
Scala Steward <me@scala-steward.org>
2020-12-18 21:06:07 +0100

    build(deps): bump msgpack-core from 0.8.21 to 0.8.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val organizeImports = "0.4.4"
 
     val shapeless   = "2.3.3"
-    val msgpackJava = "0.8.21"
+    val msgpackJava = "0.8.22"
 
     val munit = "0.7.20"
   }
