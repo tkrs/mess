e11fae02495946df8c1801e50fd5ab7c456b7196
Takeru Sato <type.in.type@gmail.com>
2021-01-04 23:21:42 +0900

    feat: set default config to default argument

--- modules/examples/src/main/scala/com/github/tkrs/examples/Main.scala
+++ modules/examples/src/main/scala/com/github/tkrs/examples/Main.scala
@@ -3,15 +3,13 @@ package com.github.tkrs.examples
 import mess.Fmt
 import mess.codec.Decoder
 import mess.codec.Encoder
-import org.msgpack.core.MessagePack.DEFAULT_PACKER_CONFIG
-import org.msgpack.core.MessagePack.DEFAULT_UNPACKER_CONFIG
 
 object Main extends App {
   val bar = Bar(Foo(2305, "Archimedes") :: Foo(411, "Johannes Kepler") :: Nil)
 
-  val packed = Fmt.pack(Encoder[Bar].apply(bar), DEFAULT_PACKER_CONFIG)
+  val packed = Fmt.pack(Encoder[Bar].apply(bar))
 
-  val unpacked = Decoder[Bar].apply(Fmt.unpack(packed, DEFAULT_UNPACKER_CONFIG))
+  val unpacked = Decoder[Bar].apply(Fmt.unpack(packed))
 
   assert(Right(bar) == unpacked)
 }
