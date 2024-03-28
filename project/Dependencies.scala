4039be088b51217d98372a7c042a045b9df3a0ef
Scala Steward <scala_steward@virtuslab.com>
2024-03-28 21:31:02 +0000

    build(deps): bump scala3-library from 3.3.3 to 3.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val Ver = new {
-    val scala3      = "3.3.3"
+    val scala3      = "3.4.1"
     val `scala2.13` = "2.13.13"
     val `scala2.12` = "2.12.19"
 
