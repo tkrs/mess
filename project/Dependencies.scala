daec9234c79b7d458944612f7c5db51825d34742
Scala Steward <scala_steward@virtuslab.com>
2024-05-17 19:30:56 +0000

    build(deps): bump shapeless from 2.3.10 to 2.3.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
 
     val organizeImports = "0.6.0"
 
-    val shapeless   = "2.3.10"
+    val shapeless   = "2.3.11"
     val msgpackJava = "0.9.8"
 
     val munit = "0.7.29"
